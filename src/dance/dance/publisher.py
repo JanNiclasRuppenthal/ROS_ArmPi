ad5b3ddd62dce6de0a93c46ebd107ac8a0284a60
Jan Niclas Ruppenthal <jan.rupp06@web.de>
2024-06-25 10:26:36 +0000

    Publish temperature to subscriber on topic temp

--- src/dance/dance/publisher.py
+++ src/dance/dance/publisher.py
@@ -1,3 +1,8 @@
+import sys
+sys.path.append('/home/pi/ArmPi/')
+
+import HiwonderSDK.Board as Board
+
 import rclpy
 from rclpy.node import Node
 
@@ -8,17 +13,18 @@ class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        self.publisher_ = self.create_publisher(String, 'temp', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+        self.i = 1
 
     def timer_callback(self):
         msg = String()
-        msg.data = 'Hello World: %d' % self.i
+        temp = Board.getBusServoTemp(self.i)
+        msg.data = 'My temp for servo %d: %d' % (self.i, temp)
         self.publisher_.publish(msg)
         self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
+        self.i = self.i % 6 + 1
 
 
 def main(args=None):

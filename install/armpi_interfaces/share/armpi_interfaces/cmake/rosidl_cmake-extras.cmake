04a1cfea7e83b272e0325a8874c1e083e97c61f8
Jan Niclas Ruppenthal <jan.rupp06@web.de>
2024-11-12 12:03:00 +0100

    Remove install folder for git

--- install/armpi_interfaces/share/armpi_interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(armpi_interfaces_IDL_FILES "msg/AssembleQueue.idl;msg/IDArmPi.idl;msg/PositionWithAngle.idl")
-set(armpi_interfaces_INTERFACE_FILES "msg/AssembleQueue.msg;msg/IDArmPi.msg;msg/PositionWithAngle.msg")

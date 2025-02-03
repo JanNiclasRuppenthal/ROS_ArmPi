d965c88c34fbb0467012f456a6d17844263d1c90
Jan Niclas Ruppenthal <jan.rupp06@web.de>
2025-02-03 13:55:02 +0100

    Renamed one package: assembly -> transfer_cubes

--- src/transfer_cubes/setup.py
+++ src/transfer_cubes/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-package_name = 'assembly'
+package_name = 'transfer_cubes'
 
 setup(
     name=package_name,
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'main = assembly.main:main'
+            'main = transfer_cubes.main:main'
         ],
     },
 )
